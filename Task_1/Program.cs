ea93afca7b71748864e2e8aeb57013d355e06249
Vladimir <vladsmir323@gmail.com>
2022-04-29 21:06:48 +0300

    Сравнение двух чисел на максимум и минимум

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+if (max > b)
+{
+   Console.WriteLine($"Первое число наибольшее = {a}, второе число наименьшее = {b}");
+}
+else
+{
+   Console.WriteLine($"Первое число наибольшее = {b}, второе число наименьшее = {a}");
+}
