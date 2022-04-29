f4644e9265ba3be9f01077e734606e76dfc706a9
Vladimir <vladsmir323@gmail.com>
2022-04-29 21:12:56 +0300

    Сравнение трех чисел и нахождение максимума

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
